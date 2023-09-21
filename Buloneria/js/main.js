36af8b3dfa264476beef7f4610d963d95ad879d1
Yoti10 <cfgyotti@gmail.com>
2023-09-21 19:54:58 -0300

    Integracion de contenido
    Integracion de pagina contacto, productos y registro,
    "mejora en nav header y menu hamburguesa"

---
+++ Buloneria/js/main.js
@@ -0,0 +1,11 @@
+const navBar = document.querySelector("#nav-header");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    navBar.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    navBar.classList.remove("visible");
+})
